87377bad5d0953f43884076d0b3dcbb769666129
Sergey Klimov <desire0417@gmail.com>
2020-08-18 09:00:33 +0300

    добавляет выделение активного типа сортировки

--- src/view/films-sorting.js
+++ src/view/films-sorting.js
@@ -1,9 +1,11 @@
 import AbstractView from './abstract.js';
 import {SortType} from '../const.js';
 
+const sortButtonActiveClass = `sort__button--active`;
+
 const createFilmsSortingTemplate = () => {
   return `<ul class="sort">
-            <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortType.BY_DEFAULT}">Sort by default</a></li>
+            <li><a href="#" class="sort__button ${sortButtonActiveClass}" data-sort-type="${SortType.BY_DEFAULT}">Sort by default</a></li>
             <li><a href="#" class="sort__button" data-sort-type="${SortType.BY_DATE}">Sort by date</a></li>
             <li><a href="#" class="sort__button" data-sort-type="${SortType.BY_RATING}">Sort by rating</a></li>
           </ul>`;
@@ -26,6 +28,13 @@ export default class FilmsSorting extends AbstractView {
 
     evt.preventDefault();
     this._callback.click(evt.target.dataset.sortType);
+    this._addActiveClass(evt.target);
+  }
+
+  _addActiveClass(target) {
+    const sortLinks = this.getElement().querySelectorAll(`.sort__button`);
+    sortLinks.forEach((it) => it.classList.remove(sortButtonActiveClass));
+    target.classList.add(sortButtonActiveClass);
   }
 
   setClickHandler(callback) {
