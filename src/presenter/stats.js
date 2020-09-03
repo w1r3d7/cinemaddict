26a92d636d9501a4a660b4eafc07cbad6b4728a3
Sergey Klimov <desire0417@gmail.com>
2020-09-03 20:18:08 +0300

    датабиндинг статистики

--- src/presenter/stats.js
+++ src/presenter/stats.js
@@ -5,17 +5,27 @@ export default class Stats {
   constructor(statsContainer, filmsModel) {
     this._statsContainer = statsContainer;
     this._statsComponent = null;
-    this._films = filmsModel.getFilms();
+    this._filmsModel = filmsModel;
+    this._films = this._filmsModel.getFilms();
+    this._handleModelAction = this._handleModelAction.bind(this);
   }
 
   init() {
+    this._filmsModel.addObserver(this._handleModelAction);
     const viewedFilms = this._films.filter((film) => film.isViewed);
     this._statsComponent = new StatsView(viewedFilms);
     render(this._statsContainer, this._statsComponent);
     this._statsComponent.setClickFilterHandler();
+
+  }
+
+  _handleModelAction() {
+    this.destroy();
+    this.init();
   }
 
   destroy() {
     removeComponent(this._statsComponent);
+    this._filmsModel.deleteObserver(this._handleModelAction);
   }
 }
